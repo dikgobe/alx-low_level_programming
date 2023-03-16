c7185a5080a819801d33e69cc7ec13b7056a782a
molepo <dikgobe>
2023-03-16 06:41:21 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include<stdi.h>
+/**
+ * main -- Aprogram that peint aline using printf function
+ * Return: 0 (succses)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
