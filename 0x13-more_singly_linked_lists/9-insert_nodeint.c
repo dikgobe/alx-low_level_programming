9fb1f877d2025eeb105183cdeae5ef2c229957c9
dikgobe <djmolepo@gmail.com>
2023-05-03 16:45:01 -0700

    insert node at an index position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -31,7 +31,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	for (index = 0; str && index < idx; index++)
 	{
-		if (i == idx - 1)
+		if (index == idx - 1)
 		{
 			ptr->next = str->next;
 			str->next = ptr;
